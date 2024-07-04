ab24f1dcd2e41aeaa96dc5332b46264ad54ef3c8
Ekaterina Pavlova <miss.kat.katerina2007@gmail.com>
2024-07-05 01:19:45 +0300

    edited main

--- src/main.py
+++ src/main.py
@@ -1,12 +1,26 @@
+import pandas as pd
+
+from src.reports import spending_by_category
+from src.services import get_transactions_by_keyword
 from src.views import main_page_info
 
 
 def main() -> None:
-    '''
+    """
     Основная функция программы для взаимодействия с пользователем.
-    '''
-    user_data = input("Введите текущую дату и время в формате YYYY-MM-DD HH:MM:SS ")
-    print(main_page_info(user_date=user_data))
+    """
+    user_data_1 = input("Введите текущую дату и время в формате YYYY-MM-DD HH:MM:SS ")
+    print(main_page_info(user_date=user_data_1))
+    print("---------- ---------- ----------")
+
+    user_data_2 = input("Введите ключевое слово для поиска ")
+    print(get_transactions_by_keyword(user_keyword=user_data_2))
+    print("---------- ---------- ----------")
+
+    transactions_df = pd.read_excel("../data/operations.xls")
+    user_data_category = input("Введите категорию ")
+    date = input("Введите дату и время в формате YYYY-MM-DD HH:MM:SS ")
+    print(spending_by_category(transactions=transactions_df, category=user_data_category, date=date))
     print("---------- ---------- ----------")
 
 
